d0a9f643812b54aa5e428b797d9db997deef1ae7
DiegoP519 <diego.1234palomeque@gmail.com>
2024-11-23 03:14:14 -0500

    septimo commit

--- scriptlog/signup.js
+++ scriptlog/signup.js
@@ -1,10 +1,25 @@
 const signupForm = document.querySelector('#signupForm')
-signupForm.addEventListener('submit', (e)=>{
+signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const name = document.querySelector('#name')
-    const email=document.querySelector('#email')
-    const password= document.querySelector('#password')
+    const name = document.querySelector('#name').value
+    const lastname = document.querySelector('#lastname').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-    const users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = users.find(user => user.email)
+    let Users
+    try {
+        Users = JSON.parse(localStorage.getItem('users')) || []
+    } catch (error) {
+        Users = []
+    }
+
+    const isUserRegistered = Users.find(user => user.email === email)
+    if (isUserRegistered) {
+        return alert('Parece que ya te has registrado')
+    }
+
+    Users.push({ name: name, lastname: lastname, email: email, password: password })
+    localStorage.setItem('users', JSON.stringify(Users))
+    alert('Felicidades! Ya estás registrado')
+    window.location.href = 'login.html'
 })
