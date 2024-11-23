a4ee38692e0472f8ee596abb0870ec44f0327291
DiegoP519 <diego.1234palomeque@gmail.com>
2024-11-22 20:23:15 -0500

    Cuarto commit

--- script/menu.js
+++ script/menu.js
@@ -5,5 +5,13 @@ console.log(menu)
 console.log(hamburguer)
 
 hamburguer.addEventListener('click', ()=>{
-    alert("click")
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer ){
+
+         menu.classList.toggle("spread")
+    }
+
 })
